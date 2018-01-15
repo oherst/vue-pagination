4bbb593c053891b1dfe6651f39791da04c8e825d
Oliver Herst <oliver.herst@googlemail.com>
2018-01-15 23:16:04 +0000

    get data, add shuffle

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,70 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <h3>Paginated table test</h3>
+
+    <button v-on:click="shuffle">Shuffle</button>
+
+    <div class="container">
+      <div>
+        <div class="numbers" v-for="n in rowsToShow.length">{{ n }}</div>
+      </div>
+
+      <transition-group name="flip-list" tag="div">
+
+        <table-row
+          v-for="item in rowsToShow"
+          :key="item.id"
+          :item="item"
+        >
+        </table-row>
+      </transition-group>
+
+    </div>
+    <div class="load-more" @click="addMore">
+      Load more...
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import _ from 'lodash';
+import TableRow from './table-row.vue';
+
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      fakeData: [],
+      rowsToShow: []
+    }
+  },
+  components: {
+    TableRow
+  },
+
+  created() {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then((response) => {
+      this.fakeData = response.data;
+    })
+    .then(this.initArray);
+  },
+
+  methods: {
+    initArray() {
+      const [...partial] = this.fakeData;
+      this.rowsToShow = partial.slice(0,10);
+    },
+    addMore() {
+      const curLen = this.rowsToShow.length;
+      const [...partial] = this.fakeData;
+      const additional = partial.slice(curLen, curLen + 10);
+
+      this.rowsToShow = [...this.rowsToShow, ...additional];
+    },
+    shuffle() {
+      this.rowsToShow = _.shuffle(this.rowsToShow);
     }
   }
 }
@@ -33,28 +73,34 @@ export default {
 <style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width: 70%;
+  margin: auto;
 }
 
-h1, h2 {
-  font-weight: normal;
+.container {
+  display: flex;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
+.numbers {
+  width: 50px;
 }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
+.numbers,
+.row {
+  height: 30px;
 }
 
-a {
-  color: #42b983;
+.flip-list-move {
+  transition: transform 1s;
+}
+
+.load-more {
+  margin: 10px;
+  padding: 10px;
+  background-color: #eee;
+  &:hover {
+    cursor: pointer;
+  }
 }
 </style>
---
+++ src/table-row.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="row">
+    {{ item.title }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['item']
+}
+</script>
+
+<style lang="scss">
+.row {
+  //border: 1px solid #eee;
+  //padding: 7px;
+}
+</style>
